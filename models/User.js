f02ab0a829341b7c3f18a79ebf07b5a5c93e1083
Olesya <pachevaol@gmail.com>
2024-06-21 14:52:41 +0300

    add admin to user.js

--- models/User.js
+++ models/User.js
@@ -1,3 +1,8 @@
+/**
+ * Класс User представляет собой модель пользователя.
+ * Содержит приватные и публичные поля для идентификации, безопасности и базовой информации о пользователе
+ */
+
 export default class User {
   #type = "user";
   #password;
@@ -5,6 +10,15 @@ export default class User {
   #email;
   firstName;
   username;
+
+  /**
+   *
+   * @param {Number} id - Идентификатор пользователя (Стринга или намбер)
+   * @param {String} username - Имя (логин)
+   * @param {String} firstName - Имя
+   * @param {String} email - Адрес электронной почты
+   * @param {String} password - Пароль
+   */
 
   constructor(id, username, firstName, email, password) {
     this.#id = id;
@@ -16,3 +30,33 @@ export default class User {
 }
 
 // Добавить админа. Методы. Приватное поле passwordToDataBase
+
+/**
+ * Класс Admin представляет собой модель пользователя, наследуемую от класса User.
+ * Добавляет приватное поле "пароль от базы данных" к базовым характеристикам пользователя.
+ */
+
+export class Admin extends User {
+  #passwordToDataBase; // Приватное поле для хранения пароля от базы данных
+  /**
+   * Создает новый экземпляр администратора.
+   * @param {Number} id - Идентификатор админа
+   * @param {String} username - Имя (логин)
+   * @param {String} firstName - Имя
+   * @param {String} email - Адрес электронной почты
+   * @param {String} password - Пароль
+   * @param {String} passwordToDataBase - Пароль от БД
+   */
+  constructor(id, username, firstName, email, password, passwordToDataBase) {
+    super(id, username, firstName, email, password);
+    this.#passwordToDataBase = passwordToDataBase;
+  }
+  /**
+   * Метод для админа: блокировка пользователя
+   * @param {String} username - Имя пользователя, которого нужно заблокировать
+   */
+
+  blockUser(username) {
+    console.log(`Пользователь ${username} заблокирован`);
+  }
+}
--- models/Video.js
+++ models/Video.js
@@ -1,3 +1,8 @@
+/**
+ * Класс Video представляем собой базовую модель видео.
+ * Содержит основные характеристики видео и методы для работы с техническими параметрами.
+ */
+
 export default class Video {
   #id;
   title;
@@ -5,10 +10,19 @@ export default class Video {
   description;
   length;
   techOpt = {
-    ratio: null,
-    width: null,
-    height: null,
+    ratio: null, // Отношение ширины к высоте
+    width: null, // Ширина видео
+    height: null, // Высота видео
   };
+  /**
+   *
+   * @param {String} id - Идентификатор видео.
+   * @param {String} title - Название видео.
+   * @param {String} link - Ссылка на видео.
+   * @param {String} description - Описание видео.
+   * @param {Number} length - Длительность видео в секундах.
+   * @param {Object} techOpt - Технические параметры видео (опционально).
+   */
 
   constructor(id, title, link, description, length, techOpt) {
     this.#id = id;
@@ -16,12 +30,23 @@ export default class Video {
     this.link = link;
     this.description = description;
     this.length = length;
-    this.techOpt = Object.assign({}, techOpt);
+    this.techOpt = Object.assign({}, techOpt); // Копирует переданные технические параметры
   }
+
+  /**
+   * Возвращает приватное поле #id.
+   * @returns {Number} - Идентификатор видео.
+   */
 
   get id() {
     return this.#id;
   }
+
+  /**
+   * Вычисляет и возвращает тип соотношения сторон видео (вертикальное или горизонтальное).
+   * Устанавливает значение поля techOpt.ratio.
+   * @returns {String} - Тип соотношения сторон видео: "vertical" или "horizontal".
+   */
 
   checkRatio() {
     this.techOpt.ratio = this.techOpt.width / this.techOpt.height;
@@ -31,17 +56,51 @@ export default class Video {
   }
 }
 
+/**
+ * Класс Film представляет собой модель фильма, наследуемую от класса Video.
+ * Добавляет оценку (рейтинг) фильма к базовым характеристикам видео.
+ */
+
 export class Film extends Video {
   rating;
+  /**
+   * Создает новый экземпляр фильма.
+   * @param {Number} id - Идентификатор фильма.
+   * @param {String} title - Название фильма.
+   * @param {String} link - Ссылка на фильм.
+   * @param {String} description - Описание фильма.
+   * @param {Number} length - Длительность фильма в секундах.
+   * @param {Number} width - Ширина фильма.
+   * @param {Number} height - Высота фильма.
+   * @param {Number} rating - Рейтинг фильма.
+   */
   constructor(id, title, link, description, length, width, height, rating) {
     super(id, title, link, description, length, width, height);
     this.rating = rating;
   }
 }
 
+/**
+ * Класс Shorts представляет собой модель короткометражного фильма, наследуемую от класса Video.
+ * Добавляет поля "лайк" и "дизлайк" к базовым характеристикам видео.
+ */
+
 export class Shorts extends Video {
   like;
   dislike;
+
+  /**
+   * Создает новый экземпляр короткометражного фильма.
+   * @param {Number} id - Идентификатор короткометражного фильма.
+   * @param {String} title - Название короткометражного фильма.
+   * @param {String} link - Ссылка на короткометражный фильм.
+   * @param {String} description - Описание короткометражного фильма.
+   * @param {Number} length - Длительность короткометражного фильма в секундах.
+   * @param {Number} width - Ширина короткометражного фильма.
+   * @param {Number} height - Высота короткометражного фильма.
+   * @param {Number} like - Количество лайков короткометражного фильма.
+   * @param {Number} dislike - Количество дизлайков короткометражного фильма.
+   */
   constructor(id, title, link, description, length, width, height, like, dislike) {
     super(id, title, link, description, length, width, height);
     this.like = like;
